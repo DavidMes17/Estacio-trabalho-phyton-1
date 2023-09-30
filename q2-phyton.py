0b7d502a3602241d59229439ab1562c484eda0e9
DavidMes17 <davidmes46@gmail.com>
2023-09-29 22:11:15 -0300

    finalizado (testado)

--- q2-phyton.py
+++ q2-phyton.py
@@ -1,16 +1,8 @@
-i = 0
 result = 0
+i = 0
 
-while (i == 1000000000):
-    result = result + i            
-    if (i == 1000000):
+while (i <= 1000000000):
+    if (i == 1000000 or i == 10000000 or i == 100000000 or i == 1000000000):
         print(result)
-
-    else if (i == 10000000):
-        print(result)
-
-    else if (i == 100000000):
-        print(result)
-
-    else if (i == 1000000000):
-        print(result)
+    i += 1
+    result += i
